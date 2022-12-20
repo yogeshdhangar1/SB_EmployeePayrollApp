4727c604a3d5a9081492245d1eda57e322b03b98
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-12-19 22:06:21 -0800

    Added: In This UC We Will use lambok library to generate getterand setter

--- src/main/java/com/example/sb_employeepayrollapp/Dto/ResponseDTO.java
+++ src/main/java/com/example/sb_employeepayrollapp/Dto/ResponseDTO.java
@@ -1,5 +1,8 @@
 package com.example.sb_employeepayrollapp.Dto;
-public class ResponseDTO {
+
+import lombok.Data;
+
+public  @Data class ResponseDTO {
     private String message;
     private Object data;
 

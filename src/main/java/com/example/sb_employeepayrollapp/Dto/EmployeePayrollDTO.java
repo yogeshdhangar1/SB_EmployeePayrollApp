4a9942d4424ef16bb434c1bc1ccb373a1a106aef
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-12-19 22:44:57 -0800

    Added: Appliying Regex Rule For Name So The Rest Call Be Validate

--- src/main/java/com/example/sb_employeepayrollapp/Dto/EmployeePayrollDTO.java
+++ src/main/java/com/example/sb_employeepayrollapp/Dto/EmployeePayrollDTO.java
@@ -1,9 +1,17 @@
 package com.example.sb_employeepayrollapp.Dto;
 
-public class EmployeePayrollDTO {
-    public String name;
-    public long salary;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
+public class EmployeePayrollDTO {
+    @NotEmpty(message = "Please Enter your firstName")
+    @Pattern(regexp = "^[A-Z]{1}[a-z]{2,}\\s{0,}$",message = "Please enter valid name")
+    public String name;
+    @Min(value = 500,message = "Minimum salary should be 500")
+    @NotNull(message = "salary field cannot be empty")
+    public long salary;
     public EmployeePayrollDTO(String name, long salary) {
         this.name = name;
         this.salary = salary;

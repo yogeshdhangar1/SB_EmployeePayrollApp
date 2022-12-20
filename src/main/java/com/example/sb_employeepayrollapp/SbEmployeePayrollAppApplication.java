7901e7f3274a52211a103a0574584079ab8575e0
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-12-19 21:12:25 -0800

    Added: Creating Employee Payroll Program

---
+++ src/main/java/com/example/sb_employeepayrollapp/SbEmployeePayrollAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.sb_employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEmployeePayrollAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SbEmployeePayrollAppApplication.class, args);
+        System.out.println("Welcome To Employee Payroll App");
+    }
+
+}
